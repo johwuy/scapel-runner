5bedf7b5a9c43b1764fcf19032dc8877f5b01e4e
Johnny Wu <johwuy@gmail.com>
2026-02-24 17:15:19 -0800

    Refactor to move navbar and Footer and ScrollToTop to RootLayout

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react'
 import { Outlet, createRootRoute } from '@tanstack/react-router'
+import NavBar from '../components/NavBar'
+import Footer from '../components/Footer'
+import ScrollToTop from '../components/ScrollToTop'
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -8,7 +11,10 @@ export const Route = createRootRoute({
 function RootComponent() {
   return (
     <React.Fragment>
+      <NavBar />
       <Outlet />
+      <Footer />
+      <ScrollToTop />
     </React.Fragment>
   )
 }
