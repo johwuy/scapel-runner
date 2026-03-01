10d377edbfe86775377a5067522d38bc93a1f447
Johnny Wu <johwuy@gmail.com>
2026-03-01 14:19:19 -0800

    Replace logo in Footer component with new SR Logo SVG

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="py-12 px-8 bg-[#111] text-white text-center">
       <div className="max-w-[1200px] mx-auto">
-        <img src="/logo.svg" alt="Scalpel Runners" className="h-8 w-auto mx-auto mb-3 opacity-80 brightness-0 invert" />
+        <img src="/SR Logo 2.svg" alt="Scalpel Runners" className="h-8 w-auto mx-auto mb-3 opacity-80 brightness-0 invert" />
         <div className="font-playfair text-[1.3rem] font-extrabold text-(--color-brand) mb-[0.4rem]">Scalpel Runners</div>
         <div className="text-xs tracking-[0.25em] uppercase text-white/40 mb-7 font-inter">Cut. Stitch. Heal.</div>
         <p className="text-[0.85rem] text-white/35 m-0 font-inter">
